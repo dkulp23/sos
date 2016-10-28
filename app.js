a530e70cf84b313ecb40488cd4b002c52b11ab9d
Dana Kulp <dkulp23@gmail.com>
2016-10-27 19:10:28 -0700

    Scaffold object constructor.

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 'use strict';
+var allLocations = [ ];
+
+var Location = function(name, hood, address, phone, days, times, restrictions) {
+  this.name = name;
+  this.hood = hood;
+  this.address = address;
+  this.phone = phone;
+  this.daysOpen = days;
+  this.timesOpen = times;
+  this.restrictions = restrictions;
+
+}
