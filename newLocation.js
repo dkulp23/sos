fa8b2b8a224d8dde09c44ff7e0c53c4fffdbb40d
Dana Kulp <dkulp23@gmail.com>
2016-11-02 20:47:43 -0700

    Added functions to handle checkbox for daily open status.

--- newLocation.js
+++ newLocation.js
@@ -5,7 +5,41 @@ function newLocationFormSubmit(event) {
   var name = event.target.name.value;
   var hood = event.target.hood.value;
   var address = event.target.address.value;
-  var days = parseInt(event.target.days.value);
+  var mondayStatus = function() {
+    if(document.querySelector('.monday').checked) {
+      return 1;
+    }
+  };
+  var tuesdayStatus = function() {
+    if(document.querySelector('.tuesday').checked) {
+      return 2;
+    }
+  };
+  var wednesdayStatus = function() {
+    if(document.querySelector('.wednesday').checked) {
+      return 3;
+    }
+  };
+  var thursdayStatus = function() {
+    if(document.querySelector('.thursday').checked) {
+      return 4;
+    }
+  };
+  var fridayStatus = function() {
+    if(document.querySelector('.friday').checked) {
+      return 5;
+    }
+  };
+  var saturdayStatus = function() {
+    if(document.querySelector('.saturday').checked) {
+      return 6;
+    }
+  };
+  var sundayStatus = function() {
+    if(document.querySelector('.sunday').checked) {
+      return 7;
+    }
+  };
   var openTime = parseInt(event.target.openTime.value);
   var closeTime = parseInt(event.target.closeTime.value);
   var restrictions = event.target.restrictions.value;
@@ -13,7 +47,15 @@ function newLocationFormSubmit(event) {
     'name' : name,
     'hood' : hood,
     'address' : address,
-    'days' : days,
+    'days' : [
+      mondayStatus(),
+      tuesdayStatus(),
+      wednesdayStatus(),
+      thursdayStatus(),
+      fridayStatus(),
+      saturdayStatus(),
+      sundayStatus()
+    ],
     'openTime' : openTime,
     'closeTime' : closeTime,
     'restrictions' : restrictions,
