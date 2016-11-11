f7f5d4ac4610a9e2162305d6ff1e3c92eae4b7fa
Dana Kulp <dkulp23@gmail.com>
2016-11-10 17:31:17 -0800

    Map now centers on clicked location and displays name and address of clicked location. Display is able to be styled by adding DOM elements with attributes.

--- googleMap.js
+++ googleMap.js
@@ -6,12 +6,19 @@ function mapMarker(event) {
   var mapGeo = new google.maps.Geocoder();
   for(var i = 0; i < allLocations.length; i++) {
     if(address === allLocations[i].address) {
+      var locName = allLocations[i].name;
       mapGeo.geocode( { 'address': address + ', Seattle, WA'}, function(results, status) {
         if (status == 'OK') {
-          // map.setCenter(results[0].geometry.location);
-          new google.maps.Marker({
+          map.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
             map: map,
-            position: results[0].geometry.location
+            position: results[0].geometry.location,
+          });
+          var infowindow = new google.maps.InfoWindow({
+            content: locName + '</br>' + address 
+          });
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
           });
         } else {
           alert('Geocode was not successful for the following reason: ' + status);
